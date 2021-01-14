62c35427065f7033a632cce02b9a0480c0b66b68
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-14 21:29:02 +0530

    Added:Creating Linkedlist to append operation

--- src/test/java/LinkedListProgram/MyLinkedListTest.java
+++ src/test/java/LinkedListProgram/MyLinkedListTest.java
@@ -7,6 +7,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MyLinkedListTest {
+	
+	@Test
+	public void givenThreeNumbersShouldPassedLinkedListTest()
+	{
+		MyNode<Integer> myFirstNode=new MyNode<>(56);
+		MyNode<Integer> mySecondNode=new MyNode<>(30);
+		MyNode<Integer> myThirdNode=new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result=myFirstNode.getNext().equals(mySecondNode) && 
+					   mySecondNode.getNext().equals(myThirdNode);
+		
+		assertTrue(result);
+	}
    	
 	@Test
 	public void givenThreeNumbersShouldPassedLinkedListTestToTop()
@@ -24,4 +38,22 @@ public class MyLinkedListTest {
 					   myLinkedList.tail.equals(myFirstNode);
 		assertTrue(result);
 	}
+
+	@Test
+    public void given3NumbersWhenAppendedToLinkedListShouldAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new  MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        assertTrue(result);
+
+	}
+
 }
