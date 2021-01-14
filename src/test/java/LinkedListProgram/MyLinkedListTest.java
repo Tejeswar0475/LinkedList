9d8743970b332a2e371130d180a50abad86a5480
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-14 21:41:28 +0530

    Added:Insertion of node between node

--- src/test/java/LinkedListProgram/MyLinkedListTest.java
+++ src/test/java/LinkedListProgram/MyLinkedListTest.java
@@ -40,20 +40,35 @@ public class MyLinkedListTest {
 	}
 
 	@Test
-    public void given3NumbersWhenAppendedToLinkedListShouldAddedToLast() {
-        MyNode<Integer> myFirstNode = new MyNode<>(56);
-        MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(70);
-        MyLinkedList myLinkedList = new  MyLinkedList();
+    public void givenThreeNumbersWhenAppendedShouldAddedToLast() {
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
+        MyLinkedList myLinkedList=new MyLinkedList();
         myLinkedList.add(myFirstNode);
         myLinkedList.append(mySecondNode);
         myLinkedList.append(myThirdNode);
         myLinkedList.printMyNodes();
         boolean result = myLinkedList.head.equals(myFirstNode) &&
-                myLinkedList.head.getNext().equals(mySecondNode) &&
-                myLinkedList.tail.equals(myThirdNode);
+                         myLinkedList.head.getNext().equals(mySecondNode) &&
+                         myLinkedList.tail.equals(myThirdNode);
         assertTrue(result);
-
 	}
+	
+	@Test
+    public void givenThreeNumbersWhenInserting() {
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
+        MyLinkedList myLinkedList=new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode,mySecondNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                		 myLinkedList.head.getNext().equals(mySecondNode) &&
+                		 myLinkedList.tail.equals(myThirdNode);
+        assertTrue(result);
+    }
 
 }
